8df8f5b545bc042952c7f066e819b7ec40ee5fed
Alex Odnoralov <alex.odnoralov@yandex.ru>
2024-08-17 23:44:50 +0300

    добавлен компонент TicketsCounter

---
+++ src/components/game/tickets-counter/tickets-counter.tsx
@@ -0,0 +1,26 @@
+import { defineComponent } from 'vue'
+import styles from './styles.module.css'
+import { useUserStore } from '@/stores/user'
+
+export const TicketsCounter = defineComponent({
+	name: 'TicketsCounter',
+	setup: () => {
+		const userStore = useUserStore()
+
+		return () => (
+			<p class={styles.ticketsCounter}>
+				{userStore.userTickets ? (
+					<>
+						You got <span class={styles.ticketsExist}>{userStore.userTickets}</span>
+						<img class={styles.ticketImg} src="/images/ticket.png" /> Tickets
+					</>
+				) : (
+					<>
+						Bro, you're <span class={styles.ticketsEmpty}>out of</span> tickets, bro!
+						<img class={styles.ticketImg} src="/images/ticket.png" />
+					</>
+				)}
+			</p>
+		)
+	}
+})
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export * from './loading'
 export * from './ui/button'
+export * from './game/tickets-counter'
