d82678297d9039a82d0baa53ecf8e4b65360fe71
Alex Odnoralov <alex.odnoralov@yandex.ru>
2024-08-18 17:02:57 +0300

    фикс верстки под webview

--- src/stores/tg-sdk.ts
+++ src/stores/tg-sdk.ts
@@ -1,14 +1,22 @@
+import { useState } from '@/utils/useState'
 import { defineStore } from 'pinia'
 import { computed } from 'vue'
 
 export const useTgSdkStore = defineStore('tgSdk', () => {
 	const tg = Telegram.WebApp
 
+	const [isExpanded, setIsExpanded] = useState(false)
+
+	tg.onEvent('viewportChanged', (e) => {
+		if (e.isStateStable) {
+			setIsExpanded(tg.isExpanded as unknown as boolean)
+		}
+	})
+
 	const user = computed(() => tg.initDataUnsafe.user)
 	const username = computed(() => user.value?.username || '')
 	const userId = computed(() => user.value?.id || 0)
 	const startParam = computed(() => tg.initDataUnsafe.start_param)
-	const isExpanded = computed(() => tg.isExpanded as unknown as boolean)
 
 	const initTgApp = () => tg.ready()
 
