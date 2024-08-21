209219e942535aa8aaa4ad3b442f0252621dea7b
Balybin <Balybin.iwan@yandex.ru>
2024-08-22 01:04:44 +0700

    fix

--- src/pages/tasks/[taskId]/task.tsx
+++ src/pages/tasks/[taskId]/task.tsx
@@ -13,28 +13,30 @@ const TaskPage = defineComponent({
 		const task = computed(() => tasksStore.tasks.find((t) => t.id === taskId.value))
 		const whenStartClicked = async () => {
 			await tasksStore.setTaskDone(taskId.value)
-			window.open(task.value?.links,'_blank');
+			window.open(task.value?.links, '_blank')
 		}
 		return () => (
-			<div class={styles.task}>
-				<div class={styles.title}>{task.value?.description}</div>
-				{/* <div class={styles.description}>{task.value?.description}</div> */}
-				<div class={styles.frame}>
-					<div class={styles.coins}>
-						<img class={styles.icon} src="/images/bro-coin.png" />
-						{task.value?.points && `${task.value.points} $BRO`}
+			<div class={styles.taskWrapper}>
+				<div class={styles.task}>
+					<div class={styles.title}>{task.value?.description}</div>
+					{/* <div class={styles.description}>{task.value?.description}</div> */}
+					<div class={styles.frame}>
+						<div class={styles.coins}>
+							<img class={styles.icon} src="/images/bro-coin.png" />
+							{task.value?.points && `${task.value.points} $BRO`}
+						</div>
+						<div class={styles.separator} />
+						<div class={styles.tickets}>
+							<img class={styles.icon} src="/images/ticket.png" />
+							{task.value?.tickets && `${task.value.tickets} Tickets`}
+						</div>
 					</div>
-					<div class={styles.separator} />
-					<div class={styles.tickets}>
-						<img class={styles.icon} src="/images/ticket.png" />
-						{task.value?.tickets && `${task.value.tickets} Tickets`}
+					<div class={styles.duration}>{task.value?.duration}</div>
+					<div class={styles.btnWrapper}>
+						<button class={styles.startButton} onClick={whenStartClicked}>
+							Start
+						</button>
 					</div>
-				</div>
-				<div class={styles.duration}>{task.value?.duration}</div>
-				<div class={styles.btnWrapper}>
-					<button class={styles.startButton} onClick={whenStartClicked}>
-						Start
-					</button>
 				</div>
 				<div class={styles.noCrooks}>
 					No crooks allowed!
