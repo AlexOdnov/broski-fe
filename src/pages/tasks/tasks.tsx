0b1541cd2a6197931069f025e59d2129079fc3c3
Balybin <Balybin.iwan@yandex.ru>
2024-08-22 00:59:16 +0700

    add task component

--- src/pages/tasks/tasks.tsx
+++ src/pages/tasks/tasks.tsx
@@ -3,15 +3,18 @@ import { computed, defineComponent } from 'vue'
 import styles from './styles.module.css'
 import { useTasksStore } from '@/stores/tasks'
 import { TaskListItem } from '@/components/tasks/task-list-item'
+import { useRouter } from 'vue-router'
 
 const TasksPage = defineComponent({
 	name: 'TasksPage',
 	setup() {
+		const router = useRouter()
 		const tasksStore = useTasksStore()
 		const uncompletedTasks = computed(() => tasksStore.tasks.filter((t) => !t.complete))
 		const completedTasks = computed(() => tasksStore.tasks.filter((t) => t.complete))
 		const taskSelected = (selectedTaskId: number) => {
 			console.log('task selected: ', selectedTaskId)
+			router.push({ name: 'task', params: { taskId: selectedTaskId } })
 		}
 		return () => (
 			<div>
