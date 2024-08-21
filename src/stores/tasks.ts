84b0a22bc80aa759721c9e5efb8e4c685c2eda4b
Alex Odnoralov <alex.odnoralov@yandex.ru>
2024-08-21 22:58:33 +0300

    get username from tg sdk

--- src/stores/tasks.ts
+++ src/stores/tasks.ts
@@ -1,25 +1,23 @@
 import { useApi } from '@/api/useApi'
 import { defineStore } from 'pinia'
-import { useUserStore } from './user'
 import { useState } from '@/utils/useState'
 import type { TasksCreateResponseItem } from '@/api/responseTypes'
+import { useTgSdkStore } from './tg-sdk'
 export const useTasksStore = defineStore('tasks', () => {
-	const userStore = useUserStore()
+	const tgStore = useTgSdkStore()
 	const api = useApi()
 
 	const [tasks, setTasks] = useState<TasksCreateResponseItem[]>([])
 
 	const getTasks = async () => {
-		if (!userStore.user?.username) return
-		const response = await api.getTasks({ username: userStore.user.username })
+		const response = await api.getTasks({ username: tgStore.username })
 		if (response?.tasks?.length > 0) {
 			setTasks(response.tasks)
 		}
 	}
 
 	const setTaskDone = async (taskId: number) => {
-		if (!userStore.user?.username) return
-		const response = await api.doneTask({ username: userStore.user.username, task_id: taskId })
+		const response = await api.doneTask({ username: tgStore.username, task_id: taskId })
 		if (response.status === 200) {
 			const task = tasks.value.find((x) => x.id === taskId)
 			if (!task) {
