2fa6809a16ac2d0c2e37e1e5b60cb8ec22193311
Alex Odnoralov <alex.odnoralov@yandex.ru>
2024-08-29 17:07:27 +0300

    after test improvements

--- src/stores/tg-sdk.ts
+++ src/stores/tg-sdk.ts
@@ -9,10 +9,14 @@ export const useTgSdkStore = defineStore('tgSdk', () => {
 	const userId = computed(() => user.value?.id || 0)
 	const startParam = computed(() => tg.initDataUnsafe.start_param)
 	const openLink = (url?: string) => {
-		url && tg.openLink(url)
+		url && tg.openTelegramLink(url)
 	}
 
-	const initTgApp = () => tg.ready()
+	const initTgApp = () => {
+		tg.ready()
+		tg.expand()
+		tg.disableVerticalSwipes()
+	}
 
 	return {
 		user,
