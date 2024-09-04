7c06b9b6367dae59e8cb29ff99ff4b7894f07f9d
Balybin <Balybin.iwan@yandex.ru>
2024-09-05 03:13:48 +0700

    reward-block now separated component

--- src/pages/tasks/[taskId]/task.tsx
+++ src/pages/tasks/[taskId]/task.tsx
@@ -1,11 +1,11 @@
 import { computed, defineComponent, ref } from 'vue'
 import { useRoute } from 'vue-router'
 
-import styles from './styles.module.css'
+import styles from './task.module.css'
 import { useTasksStore } from '@/stores/tasks'
 import { useTgSdkStore } from '@/stores/tg-sdk'
 import { useUserStore } from '@/stores/user'
-import { UiButton } from '@/components'
+import { UiButton, RewardBlock } from '@/components'
 
 const TaskPage = defineComponent({
 	name: 'TaskPage',
@@ -39,17 +39,7 @@ const TaskPage = defineComponent({
 					<img class={styles.img} src="/images/fist.png" alt="task image" />
 					<div class={styles.title}>{task.value?.title}</div>
 					<div class={styles.description}>{task.value?.description}</div>
-					<div class={styles.frame}>
-						<div class={styles.coins}>
-							<img class={styles.icon} src="/images/bro-coin.png" />
-							{task.value?.points && `${task.value.points} $BRO`}
-						</div>
-						<div class={styles.separator} />
-						<div class={styles.tickets}>
-							<img class={styles.icon} src="/images/ticket.png" />
-							{task.value?.tickets && `${task.value.tickets} Tickets`}
-						</div>
-					</div>
+					<RewardBlock coins={task.value.points} tickets={task.value.tickets} />
 					<div class={styles.duration}>{task.value?.duration}</div>
 					<div class={styles.btnWrapper}>
 						<UiButton
