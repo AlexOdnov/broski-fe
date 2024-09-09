3b71aaac4a2dd086fc0b6d70383b3b4ea797cace
Balybin <Balybin.iwan@yandex.ru>
2024-09-10 04:28:21 +0700

    add adsgram advertising

--- appConfig.d.ts
+++ appConfig.d.ts
@@ -3,5 +3,25 @@ interface Window {
 		baseUrl: string
 		botLink: string
 	}
-	initCdTma: ({id: number}) => Promise<() => Promise<void>>
+	initCdTma: ({ id: number }) => Promise<() => Promise<void>>
+	Adsgram: {
+		init: (props: InitProps) => AdController
+	}
+}
+
+interface InitProps {
+	blockId: string
+	debug?: boolean
+	debugBannerType?: 'FullscreenMedia' | 'RewardedVideo'
+}
+
+interface ShowPromiseResult {
+	done: boolean; // true if user watch till the end, otherwise false
+	description: string; // event description
+	state: 'load' | 'render' | 'playing' | 'destroy'; // banner state
+	error: boolean; // true if event was emitted due to error, otherwise false
+}
+
+interface AdController {
+	show: () => Promise<ShowPromiseResult>
 }
--- src/stores/advertising.ts
+++ src/stores/advertising.ts
@@ -1,23 +1,40 @@
 import { defineStore } from 'pinia'
+import { ref } from 'vue'
 
 export const useAdvertisingStore = defineStore('advertising', () => {
 	const showAdv = async () => {
-		if (!_showAdv) return false
 		try {
-			await _showAdv()
-			return true
+			const showResult = await adController.value?.show()
+			if(showResult?.done) {
+				return true
+			}
+			console.warn(showResult?.error)
+			return false
+		// if (!_showAdv) return false
+		// try {
+		// 	await _showAdv()
+		// 	return true
 		} catch (e) {
 			console.warn('failed when show adv', e)
 			return false
 		}
 	}
+	const adController = ref<AdController | null>(null)
 
-	let _showAdv: (() => Promise<void>) | null = null
+	// let _showAdv: (() => Promise<void>) | null = null
 
 	const init = async () => {
+		// try {
+		// 	_showAdv = await window.initCdTma({ id: '6029415' })
+		// 	console.log(_showAdv)
+		// } catch (e) {
+		// 	console.warn('failed adv init', e)
+		// }
 		try {
-			_showAdv = await window.initCdTma({ id: '6029415' })
-			console.log(_showAdv)
+			const adControllerInit = window.Adsgram.init({blockId: '1337'})
+			if(adControllerInit) {
+				adController.value = adControllerInit
+			}
 		} catch (e) {
 			console.warn('failed adv init', e)
 		}
