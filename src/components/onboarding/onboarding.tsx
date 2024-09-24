1c36de6be5d9dd2db54e8a1bd381036f2efc395e
Alex Odnoralov <alex.odnoralov@yandex.ru>
2024-09-24 22:35:10 +0300

    refactor onboarding

--- src/components/onboarding/onboarding.tsx
+++ src/components/onboarding/onboarding.tsx
@@ -1,7 +1,7 @@
 import { useUserStore } from '@/stores/user'
 import styles from './onboarding.module.css'
 import { ref, computed, defineComponent, type VNode } from 'vue'
-import { UiButton, type ButtonMod } from '../ui'
+import { UiPopup, type ButtonMod } from '../ui'
 import { useI18n } from 'vue-i18n'
 
 export const OnboardingComponent = defineComponent({
@@ -25,16 +25,17 @@ export const OnboardingComponent = defineComponent({
 						return {
 							image: './images/onboarding-1.webp',
 							title: (
-								<>
+								<div class={styles.title}>
 									<span class={styles.yellow}>{t('onboarding.yoBroski')}</span>,&nbsp;
 									{t('onboarding.welcome')}
-								</>
+								</div>
 							),
 							description: (
-								<>
-									<p class={styles.description}>{t('onboarding.funAndEarn')}</p>
-									<p class={styles.description}>{t('onboarding.findBRO')}</p>
-								</>
+								<div class={styles.description}>
+									<span>{t('onboarding.funAndEarn')}</span>
+									<span>{t('onboarding.findBRO')}</span>
+									<p class={styles.steps}>{currentStep.value}/3</p>
+								</div>
 							),
 							buttonMod: 'inverse',
 							buttonText: t('next'),
@@ -44,16 +45,17 @@ export const OnboardingComponent = defineComponent({
 						return {
 							image: './images/onboarding-2.webp',
 							title: (
-								<>
+								<div class={[styles.title, styles.titleFlex]}>
 									{t('onboarding.1game1Ticket')}&nbsp;
 									<img class={styles.icon} src="/images/ticket.webp" />
-								</>
+								</div>
 							),
 							description: (
-								<>
-									<p class={styles.description}>{t('onboarding.inNotEnough')}</p>
-									<p class={styles.description}>{t('onboarding.bringUrBros')}</p>
-								</>
+								<div class={styles.description}>
+									<span>{t('onboarding.inNotEnough')}</span>
+									<span>{t('onboarding.bringUrBros')}</span>
+									<p class={styles.steps}>{currentStep.value}/3</p>
+								</div>
 							),
 							buttonMod: 'inverse',
 							buttonText: t('onboarding.next'),
@@ -63,16 +65,17 @@ export const OnboardingComponent = defineComponent({
 						return {
 							image: './images/onboarding-3.webp',
 							title: (
-								<>
+								<div class={styles.title}>
 									<span class={styles.yellow}>{t('airdrop')}</span>&nbsp;
 									{t('onboarding.withNoVesting')}
-								</>
+								</div>
 							),
 							description: (
-								<>
-									<p class={styles.description}>{t('onboarding.stayTuned')}</p>
-									<p class={styles.description}>{t('onboarding.dontForgetEvery8Hours')}</p>
-								</>
+								<div class={styles.description}>
+									<span>{t('onboarding.stayTuned')}</span>
+									<span>{t('onboarding.dontForgetEvery8Hours')}</span>
+									<p class={styles.steps}>{currentStep.value}/3</p>
+								</div>
 							),
 							buttonMod: 'primary',
 							buttonText: t('ok'),
@@ -92,21 +95,14 @@ export const OnboardingComponent = defineComponent({
 		)
 
 		return () => (
-			<div class={styles.onboarding}>
-				<div class={styles.contentWrapper}>
-					<img class={styles.image} src={currentProperties.value.image} alt="example" />
-					<p class={styles.title}>{currentProperties.value.title}</p>
-					<p class={styles.description}>{currentProperties.value.description}</p>
-				</div>
-				<p class={styles.steps}>{currentStep.value}/3</p>
-				<UiButton
-					text={currentProperties.value.buttonText}
-					mod={currentProperties.value.buttonMod}
-					size={'lg'}
-					minWidth={'306px'}
-					whenClick={currentProperties.value.handler}
-				/>
-			</div>
+			<UiPopup
+				header={currentProperties.value.title}
+				body={currentProperties.value.description}
+				image={currentProperties.value.image}
+				buttonText={currentProperties.value.buttonText}
+				buttonMod={currentProperties.value.buttonMod}
+				whenClick={currentProperties.value.handler}
+			/>
 		)
 	}
 })
--- src/components/ui/popup/popup.tsx
+++ src/components/ui/popup/popup.tsx
@@ -1,6 +1,6 @@
 import styles from './popup.module.css'
 import { defineComponent, type PropType, type VNode } from 'vue'
-import { UiButton } from '@/components/ui'
+import { UiButton, type ButtonMod } from '@/components/ui'
 
 export const UiPopup = defineComponent({
 	name: 'UiPopup',
@@ -8,6 +8,7 @@ export const UiPopup = defineComponent({
 		header: { type: null as unknown as PropType<VNode | string>, required: false },
 		body: { type: null as unknown as PropType<VNode | string>, required: false },
 		buttonText: { type: String, default: 'ok' },
+		buttonMod: { type: String as PropType<ButtonMod>, required: false },
 		image: { type: String, required: false },
 		whenClick: { type: Function as PropType<(e: MouseEvent) => void>, required: true }
 	},
@@ -19,7 +20,7 @@ export const UiPopup = defineComponent({
 				<div class={styles.body}>{props.body}</div>
 				<UiButton
 					text={props.buttonText}
-					mod={'primary'}
+					mod={props.buttonMod}
 					size={'lg'}
 					minWidth={'280px'}
 					whenClick={props.whenClick}
