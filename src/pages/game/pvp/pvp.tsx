256d73d71fb44ac4d2d1813e7f0ef4842a1e298c
Alex Odnoralov <alex.odnoralov@yandex.ru>
2024-09-28 19:54:28 +0300

    rework routing

---
+++ src/pages/game/pvp/pvp.tsx
@@ -0,0 +1,12 @@
+import { computed, defineComponent, ref } from 'vue'
+
+import styles from './styles.module.css'
+
+const PvpPage = defineComponent({
+	name: 'PvpPage',
+	setup() {
+		return () => <div>pvp page</div>
+	}
+})
+
+export default PvpPage
