44c102b856a594bb7b495b79da80b14f91d0c2bc
Balybin <Balybin.iwan@yandex.ru>
2024-10-01 01:37:59 +0700

    pvp component WIP

--- src/pages/game/pvp/pvp.tsx
+++ src/pages/game/pvp/pvp.tsx
@@ -3,14 +3,17 @@ import { defineComponent, onMounted, ref } from 'vue'
 import styles from './pvp.module.css'
 import { EnergyCounter } from '@/components/pvp/energy-counter'
 import { usePvpStore } from '@/stores/pvp'
+import { ProfileIcon } from '@/components/icons'
+import { useTgSdkStore } from '@/stores/tg-sdk'
 
 const PvpPage = defineComponent({
 	name: 'PvpPage',
 	setup() {
 		const pvpStore = usePvpStore()
 		const loading = ref(true)
+		const tgStore = useTgSdkStore()
 		onMounted(async () => {
-			await pvpStore.loadCharacter()
+			await pvpStore.loadPvpCharacter()
 			loading.value = false
 		})
 		return () => (
@@ -18,10 +21,28 @@ const PvpPage = defineComponent({
 				{!loading.value && (
 					<EnergyCounter
 						class={styles.fullWidth}
-						currentEnergy={pvpStore.character?.energy.remaining ?? 0}
-						totalEnergy={pvpStore.character?.energy.maximum ?? 0}
+						currentEnergy={pvpStore.pvpCharacter?.energy.remaining ?? 0}
+						totalEnergy={pvpStore.pvpCharacter?.energy.maximum ?? 0}
 					/>
 				)}
+				<div class={styles.card}>
+					<div class={styles.flexRow}>
+						<div class={styles.profileIconWrapper}>
+							<ProfileIcon size={10} />
+						</div>
+						{tgStore.user?.username ?? 'user'}
+					</div>
+					<div class={[styles.you, styles.profile, styles.yellowBorder]} />
+				</div>
+				<div class={styles.card}>
+					<div class={styles.flexRow}>
+						<div class={styles.profileIconWrapper}>
+							<ProfileIcon size={10} />
+						</div>
+						{pvpStore.pvpMatch?.opponent.username ?? 'looking for enemy'}
+					</div>
+					<div class={[styles.enemy, styles.profile, styles.yellowBorder]} />
+				</div>
 			</div>
 		)
 	}
