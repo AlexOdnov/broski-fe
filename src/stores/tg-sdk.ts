da56a58177db1dee0bbe7ad1771181fea8c27c38
Alex Odnoralov <alex.odnoralov@yandex.ru>
2024-10-06 13:04:55 +0300

    new way to retry init tg sdk

--- src/stores/tg-sdk.ts
+++ src/stores/tg-sdk.ts
@@ -7,7 +7,7 @@ export const useTgSdkStore = defineStore('tgSdk', () => {
 	const sentry = useSentry()
 
 	let tg: null | TelegramWebApps.WebApp = null
-	let initTgSdkRetryCount = 4
+	let initTgSdkRetryCount = 3
 
 	const user = computed(() => tg?.initDataUnsafe?.user)
 	const startParam = computed(() => tg?.initDataUnsafe?.start_param)
@@ -39,6 +39,22 @@ export const useTgSdkStore = defineStore('tgSdk', () => {
 		}
 	}
 
+	const retryInitTgApp = (error: SentryError) => {
+		initTgSdkRetryCount -= 1
+		if (initTgSdkRetryCount > 0) {
+			document.querySelector('#tg-sdk-script-tag')?.remove()
+			const scriptTag = document.createElement('script')
+			scriptTag.src = 'https://telegram.org/js/telegram-web-app.js'
+			scriptTag.id = 'tg-sdk-script-tag'
+			scriptTag.onload = initTgApp
+			document.head.appendChild(scriptTag)
+			return
+		}
+		sentry.captureException(error, {
+			...(tg ? tg : {})
+		})
+	}
+
 	const initTgApp = () => {
 		try {
 			tg = Telegram.WebApp
@@ -46,25 +62,10 @@ export const useTgSdkStore = defineStore('tgSdk', () => {
 			tg.disableVerticalSwipes()
 			tg.ready()
 			if (!user.value) {
-				initTgSdkRetryCount -= 1
-				if (initTgSdkRetryCount > 0) {
-					initTgApp()
-					return
-				}
-				sentry.captureException(
-					new SentryError('Tg sdk error', 'Failed to get telegram user information'),
-					{ ...tg }
-				)
+				retryInitTgApp(new SentryError('Tg sdk error', 'Failed to get telegram user information'))
 			}
 		} catch (error) {
-			initTgSdkRetryCount -= 1
-			if (initTgSdkRetryCount > 0) {
-				initTgApp()
-				return
-			}
-			sentry.captureException(error, {
-				...(tg ? tg : {})
-			})
+			retryInitTgApp(error as SentryError)
 		}
 	}
 
