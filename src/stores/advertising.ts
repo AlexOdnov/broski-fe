0f4ba7977a631739e22cc3a76e8c168be940a46f
Alex Odnoralov <alex.odnoralov@yandex.ru>
2024-10-12 23:02:22 +0300

    test adsgram issue

--- src/stores/advertising.ts
+++ src/stores/advertising.ts
@@ -19,41 +19,41 @@ export const useAdvertisingStore = defineStore('advertising', () => {
 		}
 
 		// Adsgram
-		// try {
-		// 	const adControllerInit = window.Adsgram?.init({ blockId: '2897' })
-		// 	if (adControllerInit) {
-		// 		_adController.value = adControllerInit
-		// 	}
-		// } catch (e) {
-		// 	console.warn('failed adv init', e)
-		// }
+		try {
+			const adControllerInit = window.Adsgram?.init({ blockId: '2897' })
+			if (adControllerInit) {
+				_adController.value = adControllerInit
+			}
+		} catch (e) {
+			console.warn('failed adv init', e)
+		}
 	}
 
 	const showAdv = async () => {
-		// const lastAdsgramShowTime =
-		// 	window.localStorage.getItem(LAST_ADSGRAM_SHOW_TIME) != null
-		// 		? Number.parseInt(window.localStorage.getItem(LAST_ADSGRAM_SHOW_TIME) ?? '')
-		// 		: null
-		// const currentTime = new Date().getTime()
+		const lastAdsgramShowTime =
+			window.localStorage.getItem(LAST_ADSGRAM_SHOW_TIME) != null
+				? Number.parseInt(window.localStorage.getItem(LAST_ADSGRAM_SHOW_TIME) ?? '')
+				: null
+		const currentTime = new Date().getTime()
 
-		// if (
-		// 	_adController.value &&
-		// 	(lastAdsgramShowTime === null ||
-		// 		(!Number.isNaN(lastAdsgramShowTime) && currentTime - lastAdsgramShowTime > 1000 * 60 * 3))
-		// ) {
-		// 	// прошло 3 минуты, показываем Adsgram
-		// 	try {
-		// 		const showResult = await _adController.value?.show()
-		// 		if (showResult?.done) {
-		// 			window.localStorage.setItem(LAST_ADSGRAM_SHOW_TIME, new Date().getTime().toString())
-		// 			return true
-		// 		}
-		// 		console.warn(showResult?.error)
-		// 		return false
-		// 	} catch (e) {
-		// 		console.warn('error when show adsgram adv', e)
-		// 	}
-		// }
+		if (
+			_adController.value &&
+			(lastAdsgramShowTime === null ||
+				(!Number.isNaN(lastAdsgramShowTime) && currentTime - lastAdsgramShowTime > 1000 * 60 * 3))
+		) {
+			// прошло 3 минуты, показываем Adsgram
+			try {
+				const showResult = await _adController.value?.show()
+				if (showResult?.done) {
+					window.localStorage.setItem(LAST_ADSGRAM_SHOW_TIME, new Date().getTime().toString())
+					return true
+				}
+				console.warn(showResult?.error)
+				return false
+			} catch (e) {
+				console.warn('error when show adsgram adv', e)
+			}
+		}
 
 		//OnClicka
 		if (!_showAdvOnClicka) return false
