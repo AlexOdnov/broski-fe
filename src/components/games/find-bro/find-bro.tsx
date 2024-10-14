030df14eedb5e7042daa76220abf444e1f6017c7
Alex Odnoralov <alex.odnoralov@yandex.ru>
2024-10-14 20:57:43 +0300

    halloween placeholders for 3x3

--- src/components/games/find-bro/find-bro.tsx
+++ src/components/games/find-bro/find-bro.tsx
@@ -1,4 +1,4 @@
-import { computed, defineComponent, type PropType } from 'vue'
+import { computed, defineComponent, ref, type PropType } from 'vue'
 import styles from '../shared/game-styles.module.css'
 import { INITIAL_ATTEMPTS_COUNT, useFindBroGameStore } from '@/stores/find-bro-game'
 import { type ButtonMod, UiButton } from '@/components'
@@ -6,9 +6,15 @@ import { useUserStore } from '@/stores/user'
 import { useAdvertisingStore } from '@/stores/advertising'
 import { AdIcon } from '@/components/icons'
 import { GameElement } from '../shared'
-import { GameStatus, WIN_GAME_POINTS, FIELD_PLACEHOLDERS } from '@/utils/games'
+import {
+	GameStatus,
+	WIN_GAME_POINTS,
+	FIELD_PLACEHOLDERS,
+	HALLOWEEN_FIELD_PLACEHOLDERS
+} from '@/utils/games'
 import { useI18n } from 'vue-i18n'
 import { UiText } from '@/components/ui/ui-text'
+import { shuffle } from '@/utils/shuffle'
 
 export const FindBroGame = defineComponent({
 	name: 'FindBroGame',
@@ -20,6 +26,8 @@ export const FindBroGame = defineComponent({
 		const userStore = useUserStore()
 		const advStore = useAdvertisingStore()
 		const { t } = useI18n()
+
+		const placeholders = ref(shuffle(HALLOWEEN_FIELD_PLACEHOLDERS))
 
 		const topText = computed(() => {
 			switch (gameStore.gameStatus) {
@@ -58,14 +66,14 @@ export const FindBroGame = defineComponent({
 							text: `${t('claim')} ${WIN_GAME_POINTS} $bro`,
 							mod: 'inverse',
 							loading: gameStore.isGameLoading,
-							whenClick: () => gameStore.finishGame()
+							whenClick: finishGame
 						}
 					case GameStatus.Lose:
 						return {
 							text: t('nextTime'),
 							mod: 'inverse',
 							loading: gameStore.isGameLoading,
-							whenClick: () => gameStore.finishGame()
+							whenClick: finishGame
 						}
 					case GameStatus.InProgress:
 						return {
@@ -94,6 +102,11 @@ export const FindBroGame = defineComponent({
 			() => userStore.userTickets > 0 || gameStore.gameStatus !== GameStatus.Idle
 		)
 
+		const finishGame = () => {
+			gameStore.finishGame()
+			placeholders.value = shuffle(HALLOWEEN_FIELD_PLACEHOLDERS)
+		}
+
 		const whenAdvClick = async () => {
 			if ((await advStore.showAdv()) && userStore.user?.advertising.limit !== 0) {
 				await userStore.claimAdvertisingReward()
@@ -103,6 +116,7 @@ export const FindBroGame = defineComponent({
 
 		const switchToSuperGame = () => {
 			gameStore.finishGame(true)
+			placeholders.value = shuffle(HALLOWEEN_FIELD_PLACEHOLDERS)
 			props.whenSwitchToSuperGame()
 		}
 
@@ -117,7 +131,7 @@ export const FindBroGame = defineComponent({
 					{gameStore.gameField.map((el, index) => (
 						<GameElement
 							key={index}
-							placeholder={FIELD_PLACEHOLDERS[index]}
+							placeholder={placeholders.value[index]}
 							gameElement={el}
 							whenClick={() => gameStore.selectElement(index)}
 						/>
