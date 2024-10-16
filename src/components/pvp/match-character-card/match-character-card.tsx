51d613c75ef09a4008bcfd46c5b3bb15aaa3838a
Alex Odnoralov <alex.odnoralov@yandex.ru>
2024-10-16 14:36:20 +0300

    update match character card

--- src/components/pvp/match-character-card/match-character-card.tsx
+++ src/components/pvp/match-character-card/match-character-card.tsx
@@ -5,11 +5,12 @@ import {
 	LevelIcon,
 	ProfileIcon,
 	SpeedIcon,
+	StarsIcon,
 	StrengthIcon,
 	WeightIcon
 } from '@/components/icons'
 import { PowerCounter } from '@/components/pvp'
-import { UiText } from '@/components'
+import { CoinCounter, UiText } from '@/components'
 
 import styles from './match-competitioner-card.module.css'
 import { usePvpStore } from '@/stores/pvp'
@@ -34,12 +35,21 @@ export const MatchCharacterCard = defineComponent({
 				: pvpStore.pvpMatch?.opponent.power
 		)
 		const imageUrl = computed(() => {
+			const enemyImg = character.value?.premium
+				? '/images/enemy-prem.webp'
+				: '/images/enemy-halloween.webp'
+
+			const userImg = character.value?.premium
+				? '/images/user-prem.webp'
+				: '/images/user-halloween.webp'
+
 			return props.isEnemy
 				? character.value
-					? '/images/enemy-halloween.webp'
+					? enemyImg
 					: '/images/enemy_unknown-halloween.webp'
-				: '/images/user-halloween.webp'
+				: userImg
 		})
+
 		return () => (
 			<div class={styles.card}>
 				<div class={styles.userName}>
@@ -61,7 +71,15 @@ export const MatchCharacterCard = defineComponent({
 							: (tgStore.user?.username ?? 'user')}
 					</UiText>
 				</div>
-				<img src={imageUrl.value} class={[styles.avatar, !props.isEnemy && styles.yellowBorder]} />
+				<div class={styles.avatarWrapper}>
+					<img
+						src={imageUrl.value}
+						class={[styles.avatar, !props.isEnemy && styles.yellowBorder]}
+					/>
+					{character.value?.premium && (
+						<StarsIcon class={props.isEnemy ? styles.premIconEnemy : styles.premIcon} />
+					)}
+				</div>
 				<PowerCounter power={character.value?.power ?? 0} totalPower={totalPower.value} />
 				<div class={styles.flexRow}>
 					<div class={styles.iconWithNumber}>
@@ -109,6 +127,32 @@ export const MatchCharacterCard = defineComponent({
 						}) ?? '00'}
 					</div>
 				</div>
+				{character.value?.premium && (
+					<div class={styles.stats}>
+						<div class={styles.statsRow}>
+							<UiText fontSize={'12px'} color={'#797979'}>
+								PvP won:
+							</UiText>
+							<UiText fontSize={'12px'} fontWeight={700} isAccent>
+								{typeof character.value?.stats?.won === 'number'
+									? Intl.NumberFormat('en-US').format(character.value?.stats?.won)
+									: '??'}
+							</UiText>
+						</div>
+						<div class={styles.statsRow}>
+							<UiText fontSize={'12px'} color={'#797979'}>
+								loot:
+							</UiText>
+							{typeof character.value?.stats?.loot === 'number' ? (
+								<CoinCounter size={12} coins={character.value?.stats?.loot} />
+							) : (
+								<UiText fontSize={'12px'} fontWeight={700} isAccent>
+									??
+								</UiText>
+							)}
+						</div>
+					</div>
+				)}
 			</div>
 		)
 	}
