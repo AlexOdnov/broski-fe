543266b2009bbac94c47621043a92b2484ce7ba0
Alex Odnoralov <alex.odnoralov@yandex.ru>
2024-10-20 02:28:30 +0300

    fix router issue

--- src/components/icons/gamepad-icon.tsx
+++ src/components/icons/gamepad-icon.tsx
@@ -3,8 +3,7 @@ import { defineComponent } from 'vue'
 export const GamepadIcon = defineComponent({
 	name: 'GamepadIcon',
 	props: {
-		height: { type: Number, default: 18 },
-		color: { type: String, default: '#4E4F4F' }
+		height: { type: Number, default: 18 }
 	},
 	setup: (props) => {
 		return () => (
@@ -17,7 +16,7 @@ export const GamepadIcon = defineComponent({
 			>
 				<path
 					d="M16.8636 0H12.1364V1.28571H7.40909V2.57143H5.04545V3.85714H3.86364V10.2857H2.68182V11.5714H1.5V16.7143H2.68182V18H5.04545V16.7143H6.22727V15.4286H7.40909V14.1429H10.9545V12.8571H18.0455V14.1429H21.5909V15.4286H22.7727V16.7143H23.9545V18H26.3182V16.7143H27.5V11.5714H26.3182V10.2857H25.1364V3.85714H23.9545V2.57143H21.5909V1.28571H16.8636V0Z"
-					fill={props.color}
+					fill="currentColor"
 				/>
 				<path
 					d="M9.5 10H8.5V9.75H8.25V8.25H6.75V8H6.5V7H6.75V6.75H8.25V5.25H8.5V5H9.5V5.25H9.75V6.75H11.25V7H11.5V8H11.25V8.25H9.75V9.75H9.5M9.25 6V5.5H8.75V6H9.25Z"
