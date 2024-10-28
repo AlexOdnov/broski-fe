fbf7aee31162ca41a2725e76d928b7cf329f4917
Alex Odnoralov <alex.odnoralov@yandex.ru>
2024-10-28 18:13:05 +0300

    test new tg user parsing

--- src/utils/tg-parse.ts
+++ src/utils/tg-parse.ts
@@ -1,71 +1,26 @@
-import { SentryError, useSentry } from '@/services/sentry'
 import type { TelegramWebApps } from 'telegram-webapps'
-
-const sentry = useSentry()
-
-function urlSafeDecode(urlencoded: string) {
-	try {
-		urlencoded = urlencoded.replace(/\+/g, '%20')
-		return decodeURIComponent(urlencoded)
-	} catch (e) {
-		return urlencoded
-	}
-}
-
-function urlParseQueryString(queryString: string) {
-	const params: Record<string, string> = {}
-	if (!queryString.length) {
-		return params
-	}
-	const queryStringParams = queryString.split('&')
-	for (let i = 0; i < queryStringParams.length; i++) {
-		const param = queryStringParams[i].split('=')
-		if (param.length === 3 && param[1] === 'user') {
-			return {
-				user: urlSafeDecode(param[2])
-			}
-		}
-	}
-	return params
-}
-
-function urlParseHashParams(locationHash: string) {
-	locationHash = locationHash.replace(/^#/, '')
-	const params: Record<string, string> = {}
-	if (!locationHash.length) {
-		return params
-	}
-	if (locationHash.indexOf('=') < 0 && locationHash.indexOf('?') < 0) {
-		return params
-	}
-	const qIndex = locationHash.indexOf('?')
-	if (qIndex >= 0) {
-		locationHash = locationHash.substr(qIndex + 1)
-	}
-	const query_params = urlParseQueryString(locationHash)
-	for (const k in query_params) {
-		params[k] = query_params[k]
-	}
-	return params
-}
+import { retrieveLaunchParams } from '@telegram-apps/bridge'
 
 export function forceUpdateTgUser() {
-	let tgUser: null | TelegramWebApps.WebAppUser = null
-	let locationHash = ''
-	try {
-		locationHash = location.hash.toString()
-	} catch (error) {
-		console.warn(error)
+	const launchParamUser = retrieveLaunchParams().initData?.user
+	if (launchParamUser) {
+		const tgUser: TelegramWebApps.WebAppUser = {
+			id: launchParamUser.id,
+			is_bot: launchParamUser.isBot,
+			first_name: launchParamUser.firstName,
+			last_name: launchParamUser.lastName,
+			username: launchParamUser.username,
+			language_code: launchParamUser.languageCode,
+			is_premium: launchParamUser.isPremium ? launchParamUser.isPremium : undefined,
+			added_to_attachment_menu: launchParamUser.addedToAttachmentMenu
+				? launchParamUser.addedToAttachmentMenu
+				: undefined,
+			allows_write_to_pm: launchParamUser.allowsWriteToPm
+				? launchParamUser.allowsWriteToPm
+				: undefined,
+			photo_url: launchParamUser.photoUrl
+		}
+		// @ts-expect-error
+		Telegram.WebApp.initDataUnsafe.user = tgUser
 	}
-	const initParams = urlParseHashParams(locationHash)
-	try {
-		tgUser = JSON.parse(initParams.user)
-	} catch (error) {
-		console.warn(error)
-	}
-	// @ts-expect-error
-	Telegram.WebApp.initDataUnsafe.user = tgUser
-	// sentry.captureException(new SentryError('Tg sdk error', 'Forced update tg user info'), {
-	// 	...Telegram.WebApp.initDataUnsafe
-	// })
 }
